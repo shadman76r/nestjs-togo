f1fb544d92d74efa2c95e0ced3f23e8e748d6944
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-02-03 13:49:28 +0600

    final

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3001);
   console.log('Application is runnig go to the postman to check it the code is running or not');
 }
 bootstrap();
